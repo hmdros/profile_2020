21b50b10c07137e417c2fe5232a70893c872da3f
Henrique Medeiros <hmdros@gmail.com>
2020-06-11 21:46:17 -0300

    Updates

---
+++ precache-manifest.49beda46147c929ef8b7a919e645c502.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af21ed9f1b6ce2161c67f461d77d845",
+    "url": "/profile_2020/index.html"
+  },
+  {
+    "revision": "2b3b58876a2335b46c83",
+    "url": "/profile_2020/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "2b3b58876a2335b46c83",
+    "url": "/profile_2020/static/js/2.e8640460.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/profile_2020/static/js/2.e8640460.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a808963956172d8c68a3",
+    "url": "/profile_2020/static/js/main.dfb8c101.chunk.js"
+  },
+  {
+    "revision": "ac3f372e0f690e5a4c3d",
+    "url": "/profile_2020/static/js/runtime-main.c45f508c.js"
+  }
+]);
